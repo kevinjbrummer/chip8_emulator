5a47f91034b727933dec7a2e675bbe28b9822477
Kevin Brummer <k-brummer@ankh-systems.co.jp>
2024-05-07 15:35:52 +0900

    fix font header file

--- font4x5.h
+++ font4x5.h
@@ -1,5 +1,9 @@
+#ifndef Chip8_font4x5_h
+#define Chip8_font4x5_h
+
 #include <stdint.h>
 
-extern int FONT_BASE = 0;
-extern int FONT_SIZE = 5*16;
+static constexpr int FONT_BASE = 0;
+static constexpr int FONT_SIZE = 5*16;
 extern uint8_t font4x5[16];
+#endif
