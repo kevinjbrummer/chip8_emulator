ea6eff82cefc3d511251b8e776a12e75f2c489ac
kevinjbrummer <88122724+kevinjbrummer@users.noreply.github.com>
2024-05-10 20:40:26 +0900

    add cycle delay

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include "display.hpp"
 #include <stdio.h>
 #include <cstdlib>
+#include <chrono>
 
 int main(int argc, char* argv[])
 {
@@ -15,22 +16,33 @@ int main(int argc, char* argv[])
 
   Display display("Chip 8 Emulator");
 
+  int cycleDelay = 3;
+  auto lastCycleTime = std::chrono::high_resolution_clock::now();
+
   bool quit = false;
 
   while (!quit)
   {
-    if (!chip8.halt)
-    {
-      chip8.EmulateCycle();
-    }
-
-    if (chip8.drawFlag)
-    {
-      display.Update(chip8.gfx);
-      chip8.drawFlag = false;
-    }
-
     quit = display.ProcessInput(chip8.keys);
+    auto currentTime = std::chrono::high_resolution_clock::now();
+		float dt = std::chrono::duration<float, std::chrono::milliseconds::period>(currentTime - lastCycleTime).count();
+
+    if (dt > cycleDelay)
+    {
+
+      lastCycleTime = currentTime;
+      if (!chip8.halt)
+      {
+        chip8.EmulateCycle();
+      }
+
+      if (chip8.drawFlag)
+      {
+        display.Update(chip8.gfx);
+        chip8.drawFlag = false;
+      }
+    }
+
 
   }
   return 0;
